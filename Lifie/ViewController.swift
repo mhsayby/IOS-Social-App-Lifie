377666e6078cffa465ec4d1d1add1ebab09e9895
HAISONG <hm171@duke.edu>
2020-10-15 17:49:18 -0400

    Initial Commit

---
+++ Lifie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lifie
+//
+//  Created by HAISONG MEI on 10/15/20.
+//  Copyright © 2020 HAISONG MEI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
