749072917c3bc3ebf490273a3ba540c10bbe636c
HAISONG <hm171@duke.edu>
2020-10-19 21:29:25 -0400

    Add UIView extensions

---
+++ Lifie/Resource/Extensions.swift
@@ -0,0 +1,39 @@
+//
+//  Extensions.swift
+//  Lifie
+//
+//  Created by HAISONG MEI on 10/19/20.
+//  Copyright © 2020 HAISONG MEI. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    // Shorthands for UIViews
+    
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+    
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+    
+}
