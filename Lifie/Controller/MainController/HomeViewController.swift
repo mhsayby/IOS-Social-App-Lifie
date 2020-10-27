91116c98641994de3ad0cd512805b973b4203778
HAISONG <hm171@duke.edu>
2020-10-27 18:05:44 -0400

    Add iCarousel library; todo: modify lib

--- Lifie/Controller/MainController/HomeViewController.swift
+++ Lifie/Controller/MainController/HomeViewController.swift
@@ -15,6 +15,11 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let carousel = iCarousel(frame: CGRect(x: 0, y: 0, width: 300, height: 200))
+        carousel.dataSource = self
+        carousel.type = .coverFlow
+        view.addSubview(carousel)
+
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -29,5 +34,27 @@ class HomeViewController: UIViewController {
             present(loginViewController, animated: false)
         }
     }
+}
+
+extension HomeViewController: iCarouselDelegate, iCarouselDataSource {
+    
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return 10
+    }
+    
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        let imageView: UIImageView
+
+        if view != nil {
+            imageView = view as! UIImageView
+        } else {
+            imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 128, height: 128))
+        }
+
+        imageView.image = UIImage(named: "bedroom")
+
+        return imageView
+    }
+    
 }
 
